21f4f0255b48061bd0e3f102dfcb23cfb794e043
poing <brian.lavallee@invite-comm.jp>
2016-09-02 12:33:01 +0900

    Dynamic CI Environment, based on hostname.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,5 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+		$this->output->append_output($_SERVER['CI_ENV']);
+		$this->output->append_output($this->config->item('base_url'));
 	}
 }
